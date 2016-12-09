f580e4dcba94d889569473a7adebd67d9dc2113f
Bryan Pedlar <pedlar.bryan@gmail.com>
2016-12-08 21:59:18 -0500

    basic site uploader plugin

---
+++ .serverless_plugins/site-uploader.js
@@ -0,0 +1,63 @@
+'use strict';
+
+
+const BbPromise = require('bluebird');
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime');
+
+class Deploy {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+    this.hooks = {
+      'after:deploy:deploy': this.upload,
+      'site:upload:upload': () => BbPromise.bind(this)
+        .then(this.upload)
+    };
+
+    this.commands = {
+      "site": {
+        commands: {
+          upload: {
+            usage: 'Serve the WSGI application locally.',
+            lifecycleEvents: [
+              'upload',
+            ],
+          },
+        },
+      },
+    };
+
+  }
+
+  upload() {
+    const self = this;
+    const s3 = new AWS.S3(); 
+    const dirName = self.serverless.service.custom.site_dir;
+    const bucketName = self.serverless.service.custom.site_bucket;
+    if (!dirName || !bucketName){
+      self.serverless.cli.log("define site_dir and site_bucket in custom")
+      return;
+    }
+    fs.readdir(dirName, (err, files) => {
+      files.forEach(file => {
+        fs.readFile(path.join(dirName, file), 'utf8', function(err, data) {
+          if (err) throw err;
+          const params = {Bucket: 'bryanpedlar.com', Key: file, Body: data, ContentType:mime.lookup(file)  };
+          s3.putObject(params, function(err, data) {
+              if (err){
+                  self.serverless.cli.log(err)
+              }     
+              else{
+                  self.serverless.cli.log("Successfully uploaded data to myBucket/myKey");   
+              }
+           });
+        });
+      });
+    })
+  };
+}
+
+module.exports = Deploy;
