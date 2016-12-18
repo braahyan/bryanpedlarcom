28fd431c4d361d9f196f0b20828388441c255fa8
Bryan Pedlar <pedlar.bryan@gmail.com>
2016-12-17 19:46:02 -0500

    basic drawing uploads

--- handler.py
+++ handler.py
@@ -1,22 +1,41 @@
-import json
+from __future__ import print_function
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+import boto3
+import cgi
+from StringIO import StringIO
+from cgi import FieldStorage
+import mimetypes
+
+def upload_drawings(event, context):
+    s3 = boto3.resource('s3')
+
+    file_contents = event['body'].decode('base64')
+
+    field_storage = FieldStorage(
+        fp=StringIO(file_contents),
+        headers={'content-type': event['headers']['Content-Type']},
+        environ={'REQUEST_METHOD': 'POST'}
+    )
+
+    for file in field_storage['files']:
+        mimetype = mimetypes.guess_type(file.filename)
+        print(mimetype)
+        print(file.filename)
+        args = {
+            "Key": 'drawings/' + file.filename,
+            "Body": file.file.read(),
+        }
+
+        if mimetype and mimetype[0]:
+            args['ContentType'] = mimetype[0]
+
+        s3.Bucket('bryanpedlar.com').put_object(
+            **args
+        )
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": "ok!"
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
